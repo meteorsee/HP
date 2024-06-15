8c77754813a9956e1896f4ac563e8474ceaea361
meteorsee <meteorsee1108@gmail.com>
2024-06-15 10:23:46 +0800

    init

--- assets/js/updateData.js
+++ assets/js/updateData.js
@@ -16,23 +16,23 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 document.addEventListener('DOMContentLoaded', () => {
-    var updateCaseIDInput = document.querySelector("#updateCaseIDInput");
-    var enterWorkID = document.querySelector("#enterWorkID");
-    var enterReferenceNumber = document.querySelector("#enterReferenceNumber");
-    var enterTechnician = document.querySelector("#enterTechnician");
-    var enterHandler = document.querySelector("#enterHandler");
-    var enterHardwareParts = document.querySelector("#enterHardwareParts");
-    var enterHardwarePartNumber = document.querySelector("#enterHardwarePartNumber");
-    var enterHardwarePartStatus = document.querySelector("#enterHardwarePartStatus");
-    var enterAWBNumber = document.querySelector("#enterAWBNumber");
-    var enterMONumber = document.querySelector("#enterMONumber");
-    var enterSONumber = document.querySelector("#enterSONumber");
-    var enterRMANumber = document.querySelector("#enterRMANumber");
-    var enterDateArrived = document.querySelector("#enterDateArrived");
-    var enterDateCollected = document.querySelector("#enterDateCollected");
-    var updateBtn = document.querySelector("#updateBtn");
-    var removeBtn = document.querySelector("#removeBtn");
-    var formContainer = document.querySelector("#formContainer");
+    const updateCaseIDInput = document.querySelector("#updateCaseIDInput");
+    const enterWorkID = document.querySelector("#enterWorkID");
+    const enterReferenceNumber = document.querySelector("#enterReferenceNumber");
+    const enterTechnician = document.querySelector("#enterTechnician");
+    const enterHandler = document.querySelector("#enterHandler");
+    const enterHardwareParts = document.querySelector("#enterHardwareParts");
+    const enterHardwarePartNumber = document.querySelector("#enterHardwarePartNumber");
+    const enterHardwarePartStatus = document.querySelector("#enterHardwarePartStatus");
+    const enterAWBNumber = document.querySelector("#enterAWBNumber");
+    const enterMONumber = document.querySelector("#enterMONumber");
+    const enterSONumber = document.querySelector("#enterSONumber");
+    const enterRMANumber = document.querySelector("#enterRMANumber");
+    const enterDateArrived = document.querySelector("#enterDateArrived");
+    const enterDateCollected = document.querySelector("#enterDateCollected");
+    const updateBtn = document.querySelector("#updateBtn");
+    const removeBtn = document.querySelector("#removeBtn");
+    const formContainer = document.querySelector("#formContainer");
 
     function FindData() {
         if (!updateCaseIDInput.value) {
@@ -72,44 +72,48 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function UpdateData() {
-        const dbref = ref(db);
+        if (confirm("Are you sure you want to update this data?")) {
+            const dbref = ref(db);
 
-        update(child(dbref, "Data/" + updateCaseIDInput.value), {
-            WorkID: enterWorkID.value,
-            ReferenceNumber: enterReferenceNumber.value,
-            TechnicianName: enterTechnician.value,
-            HandlerName: enterHandler.value,
-            HardwareParts: enterHardwareParts.value,
-            HardwarePartNumber: enterHardwarePartNumber.value,
-            HardwarePartStatus: enterHardwarePartStatus.value,
-            AWBNumber: enterAWBNumber.value,
-            MONumber: enterMONumber.value,
-            SONumber: enterSONumber.value,
-            RMANumber: enterRMANumber.value,
-            DateArrived: enterDateArrived.value,
-            DateCollected: enterDateCollected.value
-        })
-        .then(() => {
-            alert("Data updated successfully");
-            location.reload(true); // true parameter forces reloading from the server instead of cache
-        })
-        .catch((error) => {
-            alert(error);
-        });
-    }
-
-    function RemoveData() {
-        const dbref = ref(db);
-
-        remove(child(dbref, "Data/" + updateCaseIDInput.value))
+            update(child(dbref, "Data/" + updateCaseIDInput.value), {
+                WorkID: enterWorkID.value,
+                ReferenceNumber: enterReferenceNumber.value,
+                TechnicianName: enterTechnician.value,
+                HandlerName: enterHandler.value,
+                HardwareParts: enterHardwareParts.value,
+                HardwarePartNumber: enterHardwarePartNumber.value,
+                HardwarePartStatus: enterHardwarePartStatus.value,
+                AWBNumber: enterAWBNumber.value,
+                MONumber: enterMONumber.value,
+                SONumber: enterSONumber.value,
+                RMANumber: enterRMANumber.value,
+                DateArrived: enterDateArrived.value,
+                DateCollected: enterDateCollected.value
+            })
             .then(() => {
-                alert("Data deleted successfully");
-                clearFormFields();
+                alert("Data updated successfully");
                 location.reload(true); // true parameter forces reloading from the server instead of cache
             })
             .catch((error) => {
                 alert(error);
             });
+        }
+    }
+
+    function RemoveData() {
+        if (confirm("Are you sure you want to delete this data?")) {
+            const dbref = ref(db);
+
+            remove(child(dbref, "Data/" + updateCaseIDInput.value))
+                .then(() => {
+                    alert("Data deleted successfully");
+                    clearFormFields();
+                    location.reload(true); // true parameter forces reloading from the server instead of cache
+                })
+                .catch((error) => {
+                    alert(error);
+                });
+        }
     }
 
     function clearFormFields() {
@@ -128,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
         enterDateCollected.value = "";
     }
 
-    var findBtn = document.querySelector("#findBtn");
+    const findBtn = document.querySelector("#findBtn");
     findBtn.addEventListener('click', FindData);
 
     updateBtn.addEventListener('click', UpdateData);
